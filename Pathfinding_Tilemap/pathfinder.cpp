9a1dc6da3739c7e7f469663f103bcf5a0e0d2c12
Xiangyu Chen <chenxy327@gmail.com>
2024-06-09 11:48:25 +0800

    implement rrd

--- Pathfinding_Tilemap/pathfinder.cpp
+++ Pathfinding_Tilemap/pathfinder.cpp
@@ -1,10 +1,21 @@
+#include <godot_cpp/variant/utility_functions.hpp>
+#include <godot_cpp/classes/ref.hpp>
+#include <godot_cpp/variant/dictionary.hpp>
+#include <cassert>
 #include "pathfinder.h"
-#include <godot_cpp/core/class_db.hpp>
 
 using namespace godot;
 
 void Pathfinder::_bind_methods() {
+    ClassDB::bind_method(D_METHOD("set_player_pos", "pos"), &Pathfinder::set_player_pos);
+    ClassDB::bind_method(D_METHOD("set_player_last_dir", "dir"), &Pathfinder::set_player_last_dir);
+	ClassDB::bind_method(D_METHOD("set_tilemap", "t"), &Pathfinder::set_tilemap);
+    ClassDB::bind_method(D_METHOD("pathfind", "search_id", "max_depth", "min", "max", "start", "end"), &Pathfinder::pathfind);
 
+    //testing
+    ClassDB::bind_method(D_METHOD("rrd_init", "agent_type_id", "goal_pos"), &Pathfinder::rrd_init);
+    ClassDB::bind_method(D_METHOD("rrd_resume", "agent_type_id", "goal_pos", "node_pos"), &Pathfinder::rrd_resume);
+    ClassDB::bind_method(D_METHOD("abstract_distance", "src_tile_id", "dest_tile_id"), &Pathfinder::abstract_distance);
 }
 
 Pathfinder::Pathfinder() {
@@ -13,4 +24,184 @@ Pathfinder::Pathfinder() {
 
 Pathfinder::~Pathfinder() {
 
+}
+
+Array Pathfinder::pathfind(int search_id, int max_depth, Vector2i min, Vector2i max, Vector2i start, Vector2i end) {
+    switch (search_id) {
+        case SearchId::DIJKSTRA:
+            return pathfind_dijkstra(max_depth, min, max, start, end);
+        case SearchId::ASTAR:
+            return pathfind_astar(max_depth, min, max, start, end);
+        case SearchId::IDASTAR:
+            return pathfind_idastar(max_depth, min, max, start, end);
+        default:
+            return pathfind_idastar(max_depth, min, max, start, end);
+    }
+}
+
+Array Pathfinder::pathfind_dijkstra(int max_depth, Vector2i min, Vector2i max, Vector2i start, Vector2i end) {
+    Array ans;
+    return ans;
+}
+
+Array Pathfinder::pathfind_astar(int max_depth, Vector2i min, Vector2i max, Vector2i start, Vector2i end) {
+    //if (dist to player > threshold) {
+    //    astar using abstract dist
+    //}
+    //else {
+    //    obstruct one of four player dirs or attempt merge with player
+    //}
+    Array ans;
+    return ans;
+}
+
+Array Pathfinder::pathfind_idastar(int max_depth, Vector2i min, Vector2i max, Vector2i start, Vector2i end) {
+    Array ans;
+    return ans;
+}
+
+Array Pathfinder::pathfind_ididjpastar(int max_depth, Vector2i min, Vector2i max, Vector2i start, Vector2i end) {
+    int tile_depth = 0;
+    int search_depth = 0;
+
+    Array ans;
+    return ans;
+}
+
+void Pathfinder::_ready() {
+    UtilityFunctions::print("Hello World from GDExtension\n");
+    GV = get_node<GDScript>("/root/GV");
+}
+
+void Pathfinder::set_player_pos(Vector2i pos) {
+    player_pos = pos;
+}
+
+void Pathfinder::set_player_last_dir(Vector2i dir) {
+    player_last_dir = dir;
+}
+
+void Pathfinder::set_tilemap(TileMap* t) {
+    cells = t;
+}
+
+bool Pathfinder::is_immediately_trapped(Vector2i pos) {
+    return false;
+}
+
+bool Pathfinder::can_move_in_dir(Vector2i pos, Vector2i dir) {
+    return false;
+}
+
+int Pathfinder::get_type_id(Vector2i pos) {
+    return cells->get_cell_atlas_coords(LayerId::TILE, pos).y;
+}
+
+int Pathfinder::get_tile_id(Vector2i pos) {
+    return cells->get_cell_atlas_coords(LayerId::TILE, pos).x + 1;
+}
+
+int Pathfinder::get_back_id(Vector2i pos) {
+    return cells->get_cell_atlas_coords(LayerId::BACK, pos).x;
+}
+
+bool Pathfinder::is_tile(Vector2i pos) {
+    return get_tile_id(pos) != 0;
+}
+
+bool Pathfinder::is_compatible(int type_id, int back_id) {
+    if (back_id == BackId::NONE) {
+        return true;
+    }
+    if (B_WALL_OR_BORDER.find(back_id) != B_WALL_OR_BORDER.end()) {
+        return false;
+    }
+    if (back_id == BackId::MEMBRANE) {
+        return type_id == TypeId::PLAYER;
+    }
+    //back_id in B_SAVE_OR_GOAL
+    return type_id == TypeId::PLAYER || type_id == TypeId::REGULAR;
+}
+
+void Pathfinder::rrd_init(int agent_type_id, Vector2i goal_pos) {
+    if (abstract_dists.contains(goal_pos)) {
+        return;
+    }
+
+    pq open;
+    um closed;
+    std::pair<pq, um> lists {open, closed};
+
+    if (is_compatible(agent_type_id, get_back_id(goal_pos))) {
+        AbstractNode n(goal_pos, 0);
+        open.push(n);
+    }
+
+    abstract_dists[goal_pos] = lists;
+}
+
+int Pathfinder::rrd_resume(int agent_type_id, Vector2i goal_pos, Vector2i node_pos) {
+    assert(abstract_dists.contains(goal_pos));
+
+    if (!is_compatible(agent_type_id, get_back_id(node_pos)) || !is_compatible(agent_type_id, get_back_id(goal_pos))) {
+        return std::numeric_limits<int>::max(); //unreachable
+    }
+
+    std::pair<pq, um>& lists = abstract_dists[goal_pos];
+    pq& open = lists.first;
+    um& closed = lists.second;
+
+    auto itr = closed.find(node_pos);
+    if (itr != closed.end()) {
+        return (*itr).second;
+    }
+
+    while (!open.empty()) {
+        AbstractNode n = open.top();
+        if (n.pos == node_pos) {
+            //found optimal abs_dist at node
+            //no need to backtrack so parents aren't stored
+            //don't pop n so search is resumable
+            return n.g;
+        }
+
+        open.pop();
+        if (closed.contains(n.pos)) { //remove duplicates in pq
+            continue;
+        }
+        closed[n.pos] = n.g;
+
+        int curr_tile_id = get_tile_id(n.pos);
+        for (Vector2i dir : DIRECTIONS) {
+            Vector2i next_pos = n.pos + dir;
+
+            if (closed.contains(next_pos)) { //closed is all optimal
+                continue;
+            }
+            int next_tile_id = get_tile_id(next_pos);
+            int next_g = n.g + abstract_distance(curr_tile_id, next_tile_id);
+            AbstractNode m(next_pos, next_g);
+            open.push(m);
+        }
+    }
+    return std::numeric_limits<int>::max(); //unreachable
+}
+
+int Pathfinder::abstract_distance(int src_tile_id, int dest_tile_id) {
+    if (dest_tile_id % TileId::ZERO == 0) {
+        return 1;
+    }
+    if (src_tile_id % TileId::ZERO == 0) {
+        return abstract_distance_util(0, dest_tile_id) - 1;
+    }
+    if (abs(dest_tile_id) == TILE_POW_MAX) {
+        return std::min(abstract_distance_util(src_tile_id, 0), abstract_distance_util(src_tile_id, -dest_tile_id));
+    }
+    return abstract_distance_util(src_tile_id, dest_tile_id);
+}
+
+int Pathfinder::abstract_distance_util(int tile_id1, int tile_id2) {
+    int ans = abs(tile_id1 - tile_id2) * 2;
+    int sgn_change_penalty = abs(sgn(tile_id1) - sgn(tile_id2)) * ABSTRACT_DIST_SIGN_CHANGE_PENALTY_FACTOR;
+    return ans + sgn_change_penalty;
 }
--- Pathfinding_Tilemap/pathfinder.h
+++ Pathfinding_Tilemap/pathfinder.h
@@ -1,15 +1,104 @@
 #ifndef PATHFINDER_H
 #define PATHFINDER_H
 
+#include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/classes/node.hpp>
+#include <godot_cpp/classes/tile_map.hpp>
+#include <godot_cpp/classes/gd_script.hpp>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
 
 namespace godot {
+
+//each cell is represented as | BackId (8) | TypeId (3) | TileId (5) |
+enum TileId {
+    EMPTY = 0,
+    ZERO = 16,
+};
+
+enum TypeId {
+    PLAYER = 0,
+    INVINCIBLE,
+    HOSTILE,
+    REGULAR,
+    VOID,
+};
+
+enum BackId {
+	NONE = 0,
+	BORDER_ROUND,
+	BORDER_SQUARE,
+	MEMBRANE,
+	BLACK_WALL,
+	BLUE_WALL,
+	RED_WALL,
+	SAVEPOINT,
+	GOAL,
+};
+
+enum LayerId {
+    BACK = 0,
+    TILE
+};
+
+enum ColorId {
+	ALL = 4,
+	RED = 29,
+	BLUE = 30,
+	BLACK = 31,
+	GRAY = 32,
+};
+
+enum SearchId {
+	DIJKSTRA = 0,
+	ASTAR,
+	IDASTAR,
+};
+
+struct AbstractNode {
+    Vector2i pos = Vector2i(0, 0);
+    int g = std::numeric_limits<int>::max();
+
+    AbstractNode(Vector2i position, int abs_dist) {
+        pos = position;
+        g = abs_dist;
+    }
+};
+
+struct AbstractNodeComparer {
+	bool operator() (AbstractNode first, AbstractNode second) {
+		return first.g > second.g;
+	}
+};
+
+struct Vector2iHasher {
+    size_t operator() (const Vector2i v) const {
+        size_t hash_x = std::hash<int>{}(v.x);
+        size_t hash_y = std::hash<int>{}(v.y);
+        return hash_x ^ (hash_y + 0x9e3779b9 + (hash_x << 6) + (hash_x >> 2));
+    }
+};
+
+typedef std::priority_queue<AbstractNode, std::vector<AbstractNode>, AbstractNodeComparer> pq;
+typedef std::unordered_map<Vector2i, int, Vector2iHasher> um;
+
+const std::unordered_set<int> B_WALL_OR_BORDER = {BackId::BORDER_ROUND, BackId::BORDER_SQUARE, BackId::BLACK_WALL, BackId::BLUE_WALL, BackId::RED_WALL};
+const std::unordered_set<int> B_SAVE_OR_GOAL = {BackId::SAVEPOINT, BackId::GOAL};
+const std::unordered_set<int> T_ENEMY = {TypeId::INVINCIBLE, TypeId::HOSTILE, TypeId::VOID};
+const std::vector<Vector2i> DIRECTIONS = {Vector2i(1, 0), Vector2i(0, 1), Vector2i(-1, 0), Vector2i(0, -1)};
+const int TILE_POW_MAX = 14;
+const int ABSTRACT_DIST_SIGN_CHANGE_PENALTY_FACTOR = 2;
 
 class Pathfinder : public Node {
     GDCLASS(Pathfinder, Node);
 
 private:
-    double time_passed;
+    Vector2i player_pos;
+    Vector2i player_last_dir;
+    TileMap* cells;
+    GDScript* GV;
+    std::unordered_map<Vector2i, std::pair<pq, um>, Vector2iHasher> abstract_dists; //goal_pos, [open, closed]; assume all entries are actively used
 
 protected:
     static void _bind_methods();
@@ -17,7 +106,34 @@ protected:
 public:
     Pathfinder();
     ~Pathfinder();
+    Array pathfind(int search_id, int max_depth, Vector2i min, Vector2i max, Vector2i start, Vector2i end);
+    Array pathfind_dijkstra(int max_depth, Vector2i min, Vector2i max, Vector2i start, Vector2i end);
+    Array pathfind_astar(int max_depth, Vector2i min, Vector2i max, Vector2i start, Vector2i end);
+    Array pathfind_idastar(int max_depth, Vector2i min, Vector2i max, Vector2i start, Vector2i end);
+    Array pathfind_ididjpastar(int max_depth, Vector2i min, Vector2i max, Vector2i start, Vector2i end);
+
+    void _ready() override;
+    void set_player_pos(Vector2i pos);
+    void set_player_last_dir(Vector2i dir);
+    void set_tilemap(TileMap* t);
+
+    bool is_immediately_trapped(Vector2i pos);
+    bool can_move_in_dir(Vector2i pos, Vector2i dir);
+    int get_type_id(Vector2i pos);
+    int get_tile_id(Vector2i pos);
+    int get_back_id(Vector2i pos);
+    bool is_tile(Vector2i pos);
+    bool is_compatible(int type_id, int back_id);
+
+    void rrd_init(int agent_type_id, Vector2i goal_pos);
+    int rrd_resume(int agent_type_id, Vector2i goal_pos, Vector2i node_pos);
+    int abstract_distance(int src_tile_id, int dest_tile_id);
+    int abstract_distance_util(int tile_id1, int tile_id2);
 };
+
+template <typename T> int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+}
 
 }
 
